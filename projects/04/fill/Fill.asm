6318ccc5ddaa4f612c6e5c32079349b8967d1593
thepigeonfighter <georgefabish@gmail.com>
2019-01-22 11:50:44 -0500

    Completed Project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -10,5 +10,70 @@
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
-
+// IF KBD != 0 
+// TURNSCREENBLACK
+//IF KBD == 0
+// TURNSCREENWHITE
 // Put your code here.
+
+@8192 //pixels filled
+D=A
+@n
+M =D
+
+
+(LISTENERLOOP)
+@KBD
+D=M
+@TURNSCREENBLACK
+D;JNE
+
+@TURNSCREENWHITE
+D;JEQ
+
+@LISTENERLOOP
+0;JMP
+
+(TURNSCREENBLACK)
+@iterator
+D=M
+@n
+D=D-M
+@ResetVars
+D;JGT
+@address
+A=M
+M=-1
+@iterator
+M = M+1
+@address
+M=M+1
+@TURNSCREENBLACK
+0;JMP
+
+
+(TURNSCREENWHITE)
+@iterator
+D=M
+@n
+D=D-M
+@ResetVars
+D;JGT
+@address
+A=M
+M=0
+@iterator
+M = M+1
+@address
+M=M+1
+@TURNSCREENWHITE
+0;JMP
+(ResetVars)
+@SCREEN
+D=A
+@address
+M=D
+@iterator
+M=1
+@LISTENERLOOP
+0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,44 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@R2 //Zeros out the product field
+M=0
+
+@R0 //Retrieves the first input
+D=M
+
+@END
+D;JEQ //Skips to end if input == 0
+
+@base //value to be added onto
+M=D
+
+
+@R1
+D=M
+@END
+D;JEQ //Skips to end if input == 0
+@n //times value should be added together
+M=D
+
+@i //Counter
+M=1
+(LOOP)
+@i
+D=M
+@n
+D=D-M 
+@END
+D;JGT
+@base
+D=M
+@R2
+M=D+M // add to product 
+@i
+M=M+1 // +1 to counter 
+@LOOP
+0;JMP //Restart Loop
+
+(END)
+@END
+0;JMP
